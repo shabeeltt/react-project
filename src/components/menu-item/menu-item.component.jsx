0f282c5e07fc58d524e10dd0842f5b4d640efdc5
shabeeltt <shabeeltt.official@gmail.com>
2024-09-11 15:29:56 +0530

    added styles and animation in the homepahe

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,10 +3,11 @@ import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, key, imageUrl, size }) => {
   return (
-    <div
-      style={{ backgroundImage: `url(${imageUrl})` }}
-      className={`${size} menu-item`}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className="background-image"
+      />
       <div className="content">
         <div className="title">{title}</div>
         <span className="subtitle">SHOP NOW</span>
